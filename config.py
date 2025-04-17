a8c674088c9c047a075b57dd3247d52647ccae7e
Bismark Socompi <bismark.sr.17@gmail.com>
2025-04-17 00:08:10 -0400

    update 17-04-2025

---
+++ config.py
@@ -0,0 +1,10 @@
+# config.py
+from dotenv import load_dotenv
+import os
+load_dotenv(override=True)
+
+# CONSTANTES
+RUTA_UNIDAD_ONE_DRIVE = os.getenv('RUTA_UNIDAD_ONE_DRIVE')
+RUTA_LOCAL_ONE_DRIVE = os.getenv('RUTA_LOCAL_ONE_DRIVE')
+API_AMIGOCLOUD_TOKEN_ADM = os.getenv('API_KEY_AMIGOCLOUD_TOKEN_ADM')
+RUTA_COMPLETA = os.path.join(RUTA_UNIDAD_ONE_DRIVE, RUTA_LOCAL_ONE_DRIVE)
